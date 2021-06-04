63c09cec05040d25a9ecd5938f1f980774f5ba17
harriet-the-spy <dpcairns@gmail.com>
2021-06-04 15:34:39 -0700

    add bugs

--- src/FuturamaQuotes.js
+++ src/FuturamaQuotes.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react'
-import request from 'superagent';
 
 export default class Counter extends Component {
     state = { quotes: [] }
 
     handleClick = async () => {
-        const data = await request.get('https://futuramaapi.herokuapp.com/api/quotes')
+        const data = await request.post('https://futuramaapi.herokuapp.comapi/quotes')
 
-        this.setState({ quotes: data.body });
+        this.setState({ quotes: data });
     }
 
     render() {
         return (
             <div className="center">
-                <button onClick={this.handleClick}>Fetch!</button>
-                { this.state.quotes.map(quote => 
+                <button onChange={this.handleClick}>Fetch!</button>
+                { state.quotes.map(quote => 
                     <p>
-                        <img width="30" src={quote.image} alt={quote.character} />
-                        {quote.character} 
+                        <img width="30" src={image} alt={character} />
+                        {character} 
                         says: 
-                        <em> "{quote.quote}"</em>
+                        <em> "{quote}"</em>
                     </p>)}
             </div>
         )
