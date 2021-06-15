5cca4c900b90cb67462323c3968e1436305e5eb2
harriet-the-spy <dpcairns@gmail.com>
2021-06-15 11:28:41 -0700

    add bad app

--- src/Body.js
+++ src/Body.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react'
+import request from 'superagent';
 
 export default class Body extends Component {
+    state = {
+        data: []
+    }
+
+    handleClick = async() => {
+        const data = await request.get('https://dani-bad-pokemon.herokuapp.com/chars');
+
+        this.setState({ data: data.body.results });
+    }
+
     render() {
         return (
             <main>
-                <section className="main-section">
-                    <img className="main-logo" src="alchemy-logo.png" alt="main-logo"/>
-                    <h2>
-                        Welcome to My React Sandbox!
-                    </h2>
-
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam repellendus similique consectetur voluptas facilis molestiae voluptates nobis error sapiente, ad, ipsa vero non, sint eligendi? Eos praesentium iusto magnam exercitationem.
-                        Earum sunt voluptatibus molestias, sint voluptatum quos reiciendis. Id asperiores deleniti dolores provident, eius, nostrum aperiam vel, optio qui illo corrupti in. Eum quam, est nemo porro sapiente excepturi assumenda.
-                        Rerum unde quidem maxime. Ullam, dolores labore molestias voluptatibus adipisci quod? Quaerat nihil inventore molestiae assumenda, voluptates vel necessitatibus eligendi? Accusamus ratione eligendi reprehenderit? Beatae incidunt molestias ea soluta nulla.
-                        Fugiat quae soluta voluptate mollitia praesentium modi ex illo, autem assumenda, vel aliquam fugit possimus dolorem esse corporis ipsa! Voluptatem sapiente error non, ea optio suscipit quasi maxime nobis ex?
-                        Et ipsa tenetur ullam ab nesciunt quas fugit velit impedit illum esse eligendi corporis culpa praesentium aspernatur ex sint veniam quidem, doloribus corrupti hic vitae. Eum neque ducimus omnis exercitationem?
-                    </p>
-                    <hr />
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam repellendus similique consectetur voluptas facilis molestiae voluptates nobis error sapiente, ad, ipsa vero non, sint eligendi? Eos praesentium iusto magnam exercitationem.
-                        Earum sunt voluptatibus molestias, sint voluptatum quos reiciendis. Id asperiores deleniti dolores provident, eius, nostrum aperiam vel, optio qui illo corrupti in. Eum quam, est nemo porro sapiente excepturi assumenda.
-                        Rerum unde quidem maxime. Ullam, dolores labore molestias voluptatibus adipisci quod? Quaerat nihil inventore molestiae assumenda, voluptates vel necessitatibus eligendi? Accusamus ratione eligendi reprehenderit? Beatae incidunt molestias ea soluta nulla.
-                        Fugiat quae soluta voluptate mollitia praesentium modi ex illo, autem assumenda, vel aliquam fugit possimus dolorem esse corporis ipsa! Voluptatem sapiente error non, ea optio suscipit quasi maxime nobis ex?
-                        Et ipsa tenetur ullam ab nesciunt quas fugit velit impedit illum esse eligendi corporis culpa praesentium aspernatur ex sint veniam quidem, doloribus corrupti hic vitae. Eum neque ducimus omnis exercitationem?
-                    </p>
-
-                </section>
+                <button onClick={this.handleClick}>Fetch!</button>
+                {
+                    this.state.data.map(pokemon => <div>
+                        <p>{pokemon.pokemon}</p>
+                        <img src={pokemon.url_image} alt={pokemon.pokemon} />
+                    </div>)
+                }
             </main>
         )
     }
