1d7a0bfc6b6c9a29f575895707e2c2ded32fdb36
Sean <simpson.sean@outlook.com>
2021-06-27 13:49:11 -0700

    WORKDONE: New React App; Fetch Utils added

---
+++ src/fetch-utils.js
@@ -0,0 +1,42 @@
+import request, { get } from 'superagent';
+
+const URL = "https://pacific-fortress-08495.herokuapp.com";
+
+export async function getAllCharacters() {
+    const data = await request.get(`${URL}/trek_character`);
+    return data.body;
+}
+
+export async function getOneCharacter(id) {
+    const data = await request.get(`${URL}/trek_character/:id`);
+    return data.body;
+}
+
+export async function updateCharacter(id, characterData) {
+    const data = await request
+        .put(`${URL}/trek_character/${id}`)
+        .send(chacterData)
+
+    return data.body;
+}
+
+export async function createCharacter(characterData) {
+    const data = await request
+        .post(`${URL}/trek_character`)
+        .send(characterData)
+    
+    return data.body;
+}
+
+export async function getAllFactions() {
+    const data = await request.get(`${URL}/trek_faction`)
+    return data.body;
+}
+
+export async function deleteCharacter(id) {
+    const data = await request
+        .delete(`${URL}/trek_character/${id}`);
+    
+    return data.body
+
+}
